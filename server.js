b688e9d25390f5925dc9cb9f338ad219173d4ed3
ravipun <ravipuj8@gmail.com>
2018-06-10 10:36:31 +0700

    Initial Account linnking

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+var app = express();
+
+//http route handler
+app.get('/',(req,res)=>{
+    //res.send('<h>Hello Express</h1>');
+    res.send({
+        name:'Andrew',
+        like:[
+            'Biking',
+            'Cities'
+        ]
+    });
+});
+
+app.get('/authenticate',(req,res)=>{
+    res.send('Authenticate');
+});
+
+//Bind the application to Machine
+app.listen(3000);
